1a51972111ffc846eb9b5aaa836eeffd020ce171
nguyen viet hung <viethung351997@gmail.com>
2018-05-23 18:04:06 +0700

    gfg

--- resources/Map/back.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="back" tilewidth="32" tileheight="32" tilecount="1008" columns="42">
- <image source="../../../1366x768-yankees-blue-solid-color-background.jpg" width="1366" height="768"/>
-</tileset>
